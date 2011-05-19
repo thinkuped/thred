634b906596be42349f04b502127d724a13e28d91
Nathan Summers <rock@gimp.org>
2011-05-19 14:13:35 -0400

    Removed unnecessary files

--- bak.bat
+++
@@ -1,7 +0,0 @@
-c:\x\pkzip -add c:\cw\thred\bak\t thred.cpp form.cpp hlp.cpp thred.rc resource.h thred.h
-c:\x\pkzip -add c:\cw\thred\bak\t c:\cw\htmx\htmx.cpp icon1.ico htmx.cpp *.cur
-cd bak
-c:\x\bnam >x1.bat
-call x1
-rem del x1.bat
-cd ..
